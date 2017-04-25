330e242b63ead2f29aae9cd9bb2aeb6d925696cb
odelay tomato <odelay.tomato@gmail.com>
2017-04-25 13:17:34 -0700

    Create skybackground.js

---
+++ skybackground.js
@@ -0,0 +1,84 @@
+setInterval(function() {
+  var now = new Date();
+  var hour = now.getHours();
+  //var hour = 5;
+
+  if (hour == 5) {
+    document.write('<body style="background-color: #315499;">');
+  } else if (hour == 6) {
+    document.write('<body style="background-color: #4670B4;">');
+  } else if (hour == 7) {
+    document.write('<body style="background-color: #639BD3;">');
+  } else if (hour == 8) {
+    document.write('<body style="background-color: #A0CFF2;">');
+  } else if (hour == 9) {
+    document.write('<body style="background-color: #B6E0F7;">');
+  } else if (hour == 10) {
+    document.write('<body style="background-color: #A7D1F2;">');
+  } else if (hour == 11) {
+    document.write('<body style="background-color: #9BC4E9;">');
+  } else if (hour == 12) {
+    document.write('<body style="background-color: #91B9E3;">');
+  } else if (hour == 13) {
+    document.write('<body style="background-color: #81A8D8;">');
+  } else if (hour == 14) {
+    document.write('<body style="background-color: #7399C9;">');
+  } else if (hour == 15) {
+    document.write('<body style="background-color: #6A8DBC;">');
+  } else if (hour == 16) {
+    document.write('<body style="background-color: #6773B6;">');
+  } else if (hour == 17) {
+    document.write('<body style="background-color: #5A65B0;">');
+  } else if (hour == 18) {
+    document.write('<body style="background-color: #343E8F;">');
+  } else if (hour == 19) {
+    document.write('<body style="background-color: #262A62;">');
+  } else if (hour == 20) {
+    document.write('<body style="background-color: #14173B;">');
+  } else if (hour == 21) {
+    document.write('<body style="background-color: #14132C;">');
+  } else if (hour == 22) {
+    document.write('<body style="background-color: #111422;">');
+  } else if (hour == 23) {
+    document.write('<body style="background-color: #110E18;">');
+  } else if (hour == 0) {
+    document.write('<body style="background-color: #06070A;">');
+  } else if (hour == 1) {
+    document.write('<body style="background-color: #0F1420;">');
+  } else if (hour == 2) {
+    document.write('<body style="background-color: #13182D;">');
+  } else if (hour == 3) {
+    document.write('<body style="background-color: #15233F;">');
+  } else if (hour == 4) {
+    document.write('<body style="background-color: #22345F;">');
+  }
+}, 360000);
+
+
+/*
+AM5:00 #315499;
+AM6:00 #4670B4;
+AM7:00 #639BD3;
+AM8:00 #A0CFF2;
+AM9:00 #B6E0F7;
+AM10:00 #A7D1F2;
+AM11:00 #9BC4E9;
+PM12:00 #91B9E3;
+PM1:00 #81A8D8;
+PM2:00 #7399C9;
+PM3:00 #6A8DBC;
+PM4:00 #6773B6;
+PM5:00 #5A65B0;
+PM6:00 #343E8F;
+PM7:00 #262A62;
+PM8:00 #14173B;
+PM9:00 #14132C;
+PM10:00 #111422;
+PM11:00 #110E18;
+AM12:00 #06070A;
+AM1:00 #0F1420;
+AM2:00 #13182D;
+AM3:00 #15233F;
+AM4:00 #22345F;
+
+*/
